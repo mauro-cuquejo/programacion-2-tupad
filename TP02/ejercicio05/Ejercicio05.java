4d5f36682c90e3b295355afe4d128909aeb5cf01
Mauro Cuquejo <mauro.cuquejo@gmail.com>
2025-08-25 00:29:33 -0300

    Corrección ejercicio 5. Se reemplaza do-while por while

--- TP02/ejercicio05/Ejercicio05.java
+++ TP02/ejercicio05/Ejercicio05.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class Ejercicio05 {
     public static void ejecutarEjercicio(Scanner scanner) {
         int sumaPares = 0;
-        int numero;
-        do {
-            System.out.print("Ingrese un número (0 para terminar): ");
-            numero = Integer.parseInt(scanner.nextLine());
+        System.out.print("Ingrese un número (0 para terminar): ");
+        int numero = Integer.parseInt(scanner.nextLine());
+        while (numero != 0) {
             if (numero != 0 && numero % 2 == 0) {
                 sumaPares += numero;
             }
-        } while (numero != 0);
+            System.out.print("Ingrese un número (0 para terminar): ");
+            numero = Integer.parseInt(scanner.nextLine());
+        }
         System.out.println("La suma de los números pares es: " + sumaPares);
     }
 }
