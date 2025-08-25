c0728676ac0d14dbbf0effcc85caf0ca84650f73
Mauro Cuquejo <mauro.cuquejo@gmail.com>
2025-08-25 00:24:19 -0300

    Se descomentan ejercicios. Formateo output

--- TP02/Main.java
+++ TP02/Main.java
@@ -19,46 +19,59 @@ public class Main {
         // Se crea un solo Scanner para toda la aplicación
         Scanner scanner = new Scanner(System.in);
 
-        // System.out.println("Ejercicio 1: Verificación de año bisiesto");
-        // Ejercicio01.ejecutarEjercicio(scanner);
+        System.out.println("Ejercicio 1: Verificación de año bisiesto");
+        Ejercicio01.ejecutarEjercicio(scanner);
+        System.out.println("\n\n");
 
-        // System.out.println("Ejercicio 2: Determinar el mayor de tres números");
-        // Ejercicio02.ejecutarEjercicio(scanner);
+        System.out.println("Ejercicio 2: Determinar el mayor de tres números");
+        Ejercicio02.ejecutarEjercicio(scanner);
+        System.out.println("\n\n");
 
-        // System.out.println("Ejercicio 3: Clasificación de Edad");
-        // Ejercicio03.ejecutarEjercicio(scanner);
+        System.out.println("Ejercicio 3: Clasificación de Edad");
+        Ejercicio03.ejecutarEjercicio(scanner);
+        System.out.println("\n\n");
 
-        // System.out.println("Ejercicio 4: Calculadora de Descuento según categoría");
-        // Ejercicio04.ejecutarEjercicio(scanner);
+        System.out.println("Ejercicio 4: Calculadora de Descuento según categoría");
+        Ejercicio04.ejecutarEjercicio(scanner);
+        System.out.println("\n\n");
 
-        // System.out.println("Ejercicio 5: Suma de números pares hasta cero");
-        // Ejercicio05.ejecutarEjercicio(scanner);
+        System.out.println("Ejercicio 5: Suma de números pares hasta cero");
+        Ejercicio05.ejecutarEjercicio(scanner);
+        System.out.println("\n\n");
 
         System.out.println("Ejercicio 6: Contador de números positivos, negativos y ceros");
         Ejercicio06.ejecutarEjercicio(scanner);
+        System.out.println("\n\n");
 
         System.out.println("Ejercicio 7: Validación de nota entre 0 y 10");
         Ejercicio07.ejecutarEjercicio(scanner);
+        System.out.println("\n\n");
 
         System.out.println("Ejercicio 8: Cálculo del Precio Final con impuesto y descuento");
         Ejercicio08.ejecutarEjercicio(scanner);
+        System.out.println("\n\n");
 
         System.out.println("Ejercicio 9: Cálculo de costo de envío y total de compra");
         Ejercicio09.ejecutarEjercicio(scanner);
+        System.out.println("\n\n");
 
         System.out.println("Ejercicio 10: Actualización de stock a partir de venta y recepción de productos");
         Ejercicio10.ejecutarEjercicio(scanner);
+        System.out.println("\n\n");
 
         System.out.println("Ejercicio 11: Cálculo de descuento especial usando variable global");
         Ejercicio11.ejecutarEjercicio(scanner);
+        System.out.println("\n\n");
 
         System.out.println(
                 "Ejercicio 12: Modificación de un array de precios y visualización de resultados de forma iterativa");
         Ejercicio12.ejecutarEjercicio();
+        System.out.println("\n\n");
 
         System.out.println(
                 "Ejercicio 13: Modificación de un array de precios y visualización de resultados de forma recursiva");
         Ejercicio13.ejecutarEjercicio();
+        System.out.println("\n\n");
 
         // Se cierra el Scanner al final de la aplicación
         scanner.close();
