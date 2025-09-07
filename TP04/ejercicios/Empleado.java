4c21b4ee503b288e712620c6f02a271acbcd60fd
Mauro Cuquejo <mauro.cuquejo@gmail.com>
2025-09-07 15:25:06 -0300

    Se agrega TP04

---
+++ TP04/ejercicios/Empleado.java
@@ -0,0 +1,83 @@
+package TP04.ejercicios;
+
+public class Empleado {
+    private int id;
+    private String nombre;
+    private String puesto;
+    private String salario;
+    private static int totalEmpleados;
+
+    public Empleado(int id, String nombre, String puesto, String salario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+        totalEmpleados++;
+    }
+
+    //Constructor sobrecargado con patrón telescópico
+    public Empleado(String nombre, String puesto) {
+        //Llama al constructor principal con id = totalEmpleados + 1
+        //para evitar id duplicado y salario = "0" por defecto.
+        this(totalEmpleados + 1, nombre, puesto, "0");
+    }
+
+    //Getters
+    public String getSalario() {
+        return this.salario;
+    }
+
+    public String getPuesto() {
+        return this.puesto;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    //No se agrega setter de salario porque no se debe modificar directamente
+
+    //Setters
+    public void setPuesto(String puesto) {
+        if (puesto != null && !puesto.isBlank()) {
+            this.puesto = puesto;
+        } else {
+            System.out.println("El puesto no puede estar vacío");
+        }
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //Métodos
+    public void actualizarSalario(Double porcentajeAumento) {
+        if (porcentajeAumento < 0) {
+            System.out.println("El porcentaje de aumento debe ser mayor a 0");
+        } else {
+            Double salarioActual = Double.parseDouble(this.salario);
+            this.salario = String.valueOf(salarioActual   + (salarioActual * porcentajeAumento / 100));
+        }
+    }
+
+    public void actualizarSalario(String importeAumento) {
+        if (Double.parseDouble(importeAumento) < 0) {
+            System.out.println("El importe de aumento debe ser mayor a 0");
+        } else {
+            Double salarioActual = Double.parseDouble(this.salario);
+            Double importeAumentoDouble = Double.parseDouble(importeAumento);
+            this.salario = String.valueOf(salarioActual   + importeAumentoDouble);
+        }
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Empleado{id=" + id + ", nombre=" + nombre + ", puesto=" + puesto + ", salario=" + salario + "}";
+    }
+
+    //Métodos estáticos
+    public static int mostrarTotalEmpleados() {
+        return totalEmpleados;
+    }
+}
---
+++ TP04/ejercicios/Main.java
@@ -0,0 +1,68 @@
+package TP04.ejercicios;
+
+public class Main {
+    public static void main(String[] args) {
+        // Crear Empleados y Mostrar total de empleados
+        System.out.println("Creando empleados...");
+        // Constructor con todos los parámetros
+        System.out.println("Total de empleados: " + Empleado.mostrarTotalEmpleados());
+        Empleado empleado1 = new Empleado(1, "Juan", "Gerente", "1000");
+        System.out.println("Total de empleados: " + Empleado.mostrarTotalEmpleados());
+        Empleado empleado2 = new Empleado(2, "Maria", "Secretaria", "800");
+
+        // Constructor con nombre y puesto
+        System.out.println("Total de empleados: " + Empleado.mostrarTotalEmpleados());
+        Empleado empleado3 = new Empleado("Pedro", "Jefe de Almacen");
+        System.out.println("Total de empleados: " + Empleado.mostrarTotalEmpleados());
+
+        // Mostrar empleados
+        System.out.println("\nMostrando empleados...");
+        System.out.println(empleado1);
+        System.out.println(empleado2);
+        System.out.println(empleado3);
+
+        // Actualizar salario con porcentaje
+        System.out.println("\nActualizando salarios por porcentaje...");
+        empleado1.actualizarSalario(-10.0);
+        System.out.println(empleado1);
+
+        empleado1.actualizarSalario(10.0);
+        System.out.println(empleado1);
+
+        empleado2.actualizarSalario(10.0);
+        System.out.println(empleado2);
+
+        empleado3.actualizarSalario(10.0);
+        System.out.println(empleado3);
+
+        // Actualizar salario con importe
+        System.out.println("\nActualizando salarios por importe...");
+
+        empleado1.actualizarSalario("-50.0");
+        System.out.println(empleado1);
+
+        empleado1.actualizarSalario("50.0");
+        System.out.println(empleado1);
+
+        empleado2.actualizarSalario("50.0");
+        System.out.println(empleado2);
+
+        // Actualizar salario con importe a partir del salario de otro empleado
+        empleado3.actualizarSalario(empleado2.getSalario());
+        System.out.println(empleado3);
+
+        System.out.println("\nProbando getters y setters...");
+        System.out.println(empleado1.getNombre());
+        System.out.println(empleado1.getPuesto());
+        System.out.println(empleado1.getSalario());
+
+        empleado1.setNombre("Juan Carlos");
+        System.out.println("Nombre: " + empleado1.getNombre());
+
+        empleado1.setPuesto("Gerente de Proyectos");
+        System.out.println("Puesto: " + empleado1.getPuesto());
+
+        System.out.println(empleado1);
+
+    }
+}
